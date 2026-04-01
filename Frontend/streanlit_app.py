5683b621d7569bc999c237ddfbc2c259029c6e41
yenfay <yenfaykung@gmail.com>
2026-04-02 02:05:20 +0800

    removed unnecessary files

--- Frontend/streanlit_app.py
+++
@@ -1,9 +0,0 @@
-import duckdb
-
-con = duckdb.connect("Datasets/investment.duckdb")
-top_holdings = con.execute("""
-    SELECT * FROM holdings
-    WHERE quarter = '2025Q1'
-    ORDER BY value DESC
-    LIMIT 20
-""").fetchdf()
