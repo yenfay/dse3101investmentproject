f10461ca49a726d5fef14a13bd5c68970f859f74
sarah <sarahchanpeiyu@gmail.com>
2026-04-02 17:26:05 +0800

    edit for batch_process_stock_price to run only if debug=true

--- Backend/transform/batch_run/batch_process_stock_price.py
+++ Backend/transform/batch_run/batch_process_stock_price.py
@@ -5,23 +5,24 @@ import numpy as np
 import time
 from Backend.transform.stock_market_price import main as stock_price_main
 from Backend.transform.consolidate_stock_price import main as consolidate_main
+from config import DEBUG
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s: %(message)s")
 logger = logging.getLogger(__name__)
 
 def main():
-    start_time = time.time()
-    logger.info("Starting batch process for stock price data...")
-    
-    # Step 1: Process raw stock price files and save to PRICES_DS_ROOT
-    # stock_price_main()
-    
-    # Step 2: Consolidate processed files into a single Parquet file
-    consolidate_main()
-    
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    logger.info(f"Batch process completed in {elapsed_time:.2f} seconds.")
+    if not DEBUG: # Only run the batch process in production mode
+        start_time = time.time()
+        logger.info("Starting batch process for stock price data...")
+        # Step 1: Process raw stock price files and save to PRICES_DS_ROOT
+        stock_price_main()
+        
+        # Step 2: Consolidate processed files into a single Parquet file
+        consolidate_main()
+        
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        logger.info(f"Batch process completed in {elapsed_time:.2f} seconds.")
 
 if __name__ == "__main__":
     main()
